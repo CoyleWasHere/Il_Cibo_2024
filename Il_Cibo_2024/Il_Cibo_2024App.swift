c22541ee55fe3fb0a705b95ac5f7ecd1cca31e24
CoyleWasHere <dancoyle321@gmail.com>
2024-01-06 19:08:56 -0500

    Initial Commit

---
+++ Il_Cibo_2024/Il_Cibo_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Il_Cibo_2024App.swift
+//  Il_Cibo_2024
+//
+//  Created by Daniel Coyle on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Il_Cibo_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
