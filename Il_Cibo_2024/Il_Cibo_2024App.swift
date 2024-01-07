3942f5399d39d81eafa1658365d9ea38bb24f8c5
CoyleWasHere <dancoyle321@gmail.com>
2024-01-07 16:07:15 -0500

    Install Pods Connect Firebase Successfully

--- Il_Cibo_2024/Il_Cibo_2024App.swift
+++ Il_Cibo_2024/Il_Cibo_2024App.swift
@@ -5,13 +5,32 @@
 //  Created by Daniel Coyle on 1/6/24.
 //
 
+
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    return true
+  }
+}
 
 @main
 struct Il_Cibo_2024App: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  
+  init() {
+      // Use Firebase library to configure APIs
+      FirebaseApp.configure()
+  }
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Il_Cibo_2024' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Il_Cibo_2024
+
+  pod 'FirebaseUI'
+  pod 'FirebaseAnalytics'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+  pod 'Firebase/Storage'
+
+end
